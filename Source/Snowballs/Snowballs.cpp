b58480156f6f2789ff1c97e4e980bee1d8aab8eb
IvannaB <ivankamorkai.baglay@gmail.com>
2023-06-13 16:37:19 +0300

    Add project files.

---
+++ Source/Snowballs/Snowballs.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Snowballs.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Snowballs, "Snowballs" );
